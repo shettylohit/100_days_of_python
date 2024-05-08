e32063f4bbd75451673f008896bc030ae125a641
shettylohit <lohit321shetty@gmail.com>
2024-05-08 05:58:02 +0530

    updated day6

---
+++ day7/rough.py
@@ -0,0 +1,17 @@
+
+# 4-4. One Million: Make a list of the numbers from one to one million, and then
+# use a for loop to print the numbers. (If the output is taking too long, stop it by
+# pressing CTRL-C or by closing the output window.)
+
+
+number=[value**3 for value in range(1,11)]
+print(number)
+
+
+
+# 4-8. Cubes: A number raised to the third power is called a cube. For example,
+# the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
+# is, the cube of each integer from 1 through 10), and use a for loop to print out
+# the value of each cube.
+# 4-9. Cube Comprehension: Use a list comprehension to generate a list of the first
+# 10 cubes.
